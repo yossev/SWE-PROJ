b81905e045d1970ca5ef8ce7ad8bddb27e38fb07
Jannahtarek91 <jannah.ouda@student.giu-uni.de>
2024-11-21 21:39:07 +0200

    Create module-schema.js

---
+++ backend/models/module-schema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const schemaOptions = {
+  timestamps: true,
+};
+
+const modulesSchema = new mongoose.Schema(
+  {
+    module_id: {
+      type: String,
+      minLength: 1,
+      required: true,
+      unique: true,
+    },
+    course_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    title: {
+      type: String,
+      minLength: 1,
+      maxLength: 100,
+      required: true,
+    },
+    content: {
+      type: String,
+      minLength: 1,
+      maxLength: 5000,
+      required: true,
+    },
+    resources: {
+      type: [String],
+      required: false,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+  },
+  schemaOptions
+);
+
+module.exports = mongoose.model('Module', module-schema);
