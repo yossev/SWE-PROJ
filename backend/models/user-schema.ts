d3dc77dc45dfc790dbaee566cad21483ad29a264
habibadarwish04 <habiba.darwish@student.giu-uni.de>
2024-11-23 23:31:32 +0200

    user ts

---
+++ backend/models/user-schema.ts
@@ -0,0 +1,35 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class User
+ {
+    @Prop({type:String,required:true,unique:true,minLength:1})
+    user_id: String;
+    @Prop({type:String,required:true,minLength:3,maxLength:50})
+    name: String;
+    @Prop({type:String,
+        minLength: 10,
+        maxLength: 50,
+        required:true})
+        email:String;
+    @Prop({type:String,
+        minLength:5,
+        required:true})
+    password_hash:String;
+    @Prop({type:String,
+        enum:['student','instructor','admin'],
+        required:true})
+        role:String;
+    @Prop({type:String,
+        minLength:10})
+    profile_picture_url:String;
+    @Prop({type:Date,
+        default:Date.now,
+        required:true})
+    created_at:Date;
+        
+  }
+
+  export const UserSchema = SchemaFactory.createForClass(User);
