0f1f39c508ac14fddf17294c617fc7aa187765ba
amanighonim <amani.ghonim@student.giu-uni.de>
2024-11-24 19:15:11 +0200

    progress.ts

--- backend/models/progress-schema.ts
+++ backend/models/progress-schema.ts
@@ -1,18 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+export type CourseDocument = HydratedDocument<Progress>;
+
 const url = "mongodb://localhost:27017/"
 mongoose.connect(url).then((ans) => { 
     console.log("Connecting SuccesFul!") 
   }).catch((err) => { 
     console.log("Error in the Connection") 
   })
+  
 @Schema()
-export class Progress {
-  @Prop({ type: String, required: true, unique: true, minLength: 1 })
-  progress_id: string;
+
+export class Progress { 
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
-  user_id: mongoose.Schema.Types.ObjectId; 
+  user_id: mongoose.Schema.Types.ObjectId;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true })
   course_id: mongoose.Schema.Types.ObjectId; 
