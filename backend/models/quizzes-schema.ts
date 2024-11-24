5fdb3ea09becd0c0e9671acd3600bf0f5892079e
salmahicham1 <salmahicham16@gmail.com>
2024-11-24 18:13:51 +0200

    removed id from quizzes

---
+++ backend/models/quizzes-schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type QuizDocument = HydratedDocument<Quiz>;
+
+
+@Schema()
+export class Quiz {
+
+  @Prop({ type: String, required: true })
+  module_id: string;
+
+  @Prop({
+    type: [
+      {
+        question: { type: String, required: true },
+        options: { type: [String], required: true }, 
+        correct_answer: { type: String, required: true }, 
+      },
+    ],
+    required: true,
+  })
+  questions: Array<{
+    question: string;
+    options: string[];
+    correct_answer: string;
+  }>;
+
+  @Prop({ type: Date, default: new Date(), required: true })
+  created_at: Date;
+}
+
+export const QuizSchema = SchemaFactory.createForClass(Quiz);
