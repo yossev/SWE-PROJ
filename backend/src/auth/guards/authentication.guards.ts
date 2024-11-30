42d4e091789ac5996156083636d64f7c52ce5382
Jannahtarek91 <jannah.ouda@student.giu-uni.de>
2024-11-30 14:33:14 +0200

    guards and middleware

---
+++ backend/src/auth/guards/authentication.guards.ts
@@ -0,0 +1,54 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import * as dotenv from 'dotenv';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+
+
+dotenv.config();
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService,private reflector: Reflector) { }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+          ]);
+          if (isPublic) {
+            return true;
+          }
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token) {
+            throw new UnauthorizedException('No token, please login');
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: process.env.JWT_SECRET
+                }
+            );
+            // 💡 We're assigning the payload to the request object here
+            // so that we can access it in our route handlers
+            request['user'] = payload;
+        } catch {
+            throw new UnauthorizedException('invalid token');
+        }
+        return true;
+    }
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const token = request.cookies?.token || request.headers['authorization']?.split(' ')[1];
+
+        return token;
+    }
+}
---
+++ backend/src/auth/guards/authorization.guards.ts
@@ -0,0 +1,31 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role, ROLES_KEY } from '../decorators/roles.decorators';
+
+
+
+
+
+
+
+@Injectable()
+export class authorizationGaurd implements CanActivate {
+  constructor(private reflector: Reflector) { }
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+      const { user } = context.switchToHttp().getRequest();
+      if(!user)
+        throw new UnauthorizedException('no user attached');
+      const userRole = user.role
+      if (!requiredRoles.includes(userRole)) 
+        throw new UnauthorizedException('unauthorized access');
+       
+    return true;
+  }
+}
