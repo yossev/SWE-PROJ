365185a3348f2a8adf61ebbb5bae1a2dcff43e04
synthpyt <synthpyt@gmail.com>
2024-12-17 21:19:00 +0200

    Added Course DELETE/UPDATE ( update still needs work )

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              src/app/page.tsx
             </code>
             .
           </li>
@@ -38,6 +38,14 @@ export default function Home() {
               height={20}
             />
             Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="/courses"
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            Go to Course
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
