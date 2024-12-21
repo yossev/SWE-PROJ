ef0653e7ee2d725f10010cce18eff01d9f690e19
salmahicham1 <salmahicham16@gmail.com>
2024-12-21 19:51:46 +0200

    added forum frontend

--- backend/src/progress/progress.module.ts
+++ backend/src/progress/progress.module.ts
@@ -10,29 +10,29 @@ import { QuizSchema } from '../../src/models/quizzes-schema';
 import {ModuleSchema } from '../../src/models/module-schema';
 import { RatingSchema } from '../../src/models/rating-schema';
 import { RatingService } from '../rating/rating.service';
-import { QuizModule } from '../quiz/quiz.module';
+
 import { JwtService } from '@nestjs/jwt';
 import { ResponseService } from 'src/response/response.service';
-import { Course } from 'models/course-schema';
+
 import { CourseService } from 'src/course/course.service';
 import { QuizService } from 'src/quiz/quiz.service';
 import { ModuleService } from 'src/module/module.service';
 import { UserSchema } from 'src/models/user-schema';
 import { ForumSchema } from 'models/forum-schema';
-import { ReplySchema } from 'models/reply-schema';
-import { ThreadSchema } from 'models/thread-schema';
-import { ForumService } from 'src/forum/forum.service';
-import { ThreadService } from 'src/thread/thread.service';
-import { ReplyService } from 'src/reply/reply.service';
-import { NotificationService } from 'src/notification/notification.service';
+
 import { NotificationSchema } from 'models/notification-schema';
-import { MessageSchema } from 'models/message-schema';
+import { NotificationService } from 'src/notification/notification.service';
 import { MessageService } from 'src/chat/message.service';
+import { ForumService } from 'src/forum/forum.service';
 import { QuestionBankSchema } from 'models/questionbank-schema';
-import { QuestionBankService } from 'src/questionbank/questionbank.service';
+import { MessageSchema } from 'models/message-schema';
 import { UserService } from 'src/user/user.service';
 import { RoomSchema } from 'models/room-schema';
 import { RoomService } from 'src/room/room.service';
+import { ThreadSchema } from 'models/thread-schema';
+import { ThreadService } from 'src/thread/thread.service';
+import { ReplySchema } from 'models/reply-schema';
+import { ReplyService } from 'src/reply/reply.service';
 import { AuthService } from 'src/auth/auth.service';
 @Module({
   imports: [
@@ -44,20 +44,24 @@ import { AuthService } from 'src/auth/auth.service';
       { name: 'Module', schema: ModuleSchema},
       {name: 'Rating', schema: RatingSchema},
       {name: 'User', schema: UserSchema},
-      {name:"Forum",schema:ForumSchema},
-           {name:"Thread",schema:ThreadSchema},
-           {name:"Reply",schema:ReplySchema},
-           { name: 'UserNotification', schema: NotificationSchema },
-           {name:"Message",schema:MessageSchema},
-           {name:'QuestionBank',schema:QuestionBankSchema},
-           {name:'Room',schema:RoomSchema}
-           
+      {name: 'Forum', schema: ForumSchema},
+      {name:'UserNotification', schema:NotificationSchema},
+      {name:'QuestionBank', schema:QuestionBankSchema},
+      {name:'Message', schema:MessageSchema},
+      {name:'Room', schema:RoomSchema},
+      {name:'Reply', schema:ReplySchema},
+      {name:'Thread', schema:ThreadSchema},
+
+      
+
     ]), 
+
+
 
   ],
   controllers: [ProgressController],
-  providers: [ProgressService, RatingService,JwtService,ResponseService,CourseService,QuizService,ModuleService,ForumService,ThreadService,
-    ReplyService,NotificationService,MessageService,QuestionBankService,UserService,RoomService,AuthService
+  providers: [ProgressService, RatingService,JwtService,ResponseService,CourseService,QuizService,ModuleService,NotificationService,
+    MessageService,ForumService,UserService,RoomService,ThreadService,ReplyService,AuthService
   ],
   exports: [ProgressService],
 })
